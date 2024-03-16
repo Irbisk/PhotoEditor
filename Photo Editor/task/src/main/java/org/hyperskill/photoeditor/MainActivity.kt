5394b292952fb9a3b8ca117bada50fcdc28ae2f4
Irbisk <Irbis2496@ya.ru>
2024-03-16 14:43:06 +0300

    Stage 2

--- Photo Editor/task/src/main/java/org/hyperskill/photoeditor/MainActivity.kt
+++ Photo Editor/task/src/main/java/org/hyperskill/photoeditor/MainActivity.kt
@@ -3,15 +3,19 @@ package org.hyperskill.photoeditor
 import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.graphics.Color
-import android.graphics.ImageDecoder
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
 import android.widget.Button
 import android.widget.ImageView
 import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.slider.Slider
 
 
 class MainActivity : AppCompatActivity() {
@@ -23,13 +27,18 @@ class MainActivity : AppCompatActivity() {
             if (result.resultCode == Activity.RESULT_OK) {
                 val photoUri = result.data?.data ?: return@registerForActivityResult
                 currentImage.setImageURI(photoUri)
-
+                defaultImage = drawableToBitmap(currentImage.drawable)
             }
         }
+
+    private lateinit var defaultImage: Bitmap
     private lateinit var currentImage: ImageView
-    private lateinit var button: Button
+    private lateinit var galleryBtn: Button
+    private lateinit var saveBtn: Button
+    private lateinit var brightnessSlider: Slider
 
 
+    @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -37,16 +46,72 @@ class MainActivity : AppCompatActivity() {
 
         //do not change this line
         currentImage.setImageBitmap(createBitmap())
-
-        button.setOnClickListener {
+        defaultImage = drawableToBitmap(currentImage.drawable)
+        galleryBtn.setOnClickListener {
             activityResultLauncher.launch(Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI))
-
         }
+
+
+
+        brightnessSlider.addOnChangeListener { slider, value, fromUser ->
+            val bitmap = defaultImage.copy(Bitmap.Config.ARGB_8888, true)
+            for (x in 0 until bitmap.width) {
+                for (y in 0 until bitmap.height) {
+                    val pixel = bitmap.getPixel(x, y)
+
+                    val alpha = Color.alpha(pixel)
+                    val red = Color.red(pixel)
+                    val green = Color.green(pixel)
+                    val blue = Color.blue(pixel)
+
+                    val colorNew = Color.argb(alpha, getRightColor(red, value), getRightColor(green, value), getRightColor(blue, value))
+                    bitmap.setPixel(x, y, colorNew)
+                }
+            }
+            currentImage.setImageBitmap(bitmap)
+        }
+
+
+    }
+
+    private fun getRightColor(color: Int, value: Float): Int {
+        var colorResult = color + value.toInt()
+        if (colorResult > 255) colorResult = 255
+        else if (colorResult < 0) colorResult = 0
+        return colorResult
+    }
+
+
+    fun drawableToBitmap(drawable: Drawable): Bitmap {
+        if (drawable is BitmapDrawable) {
+            val bitmapDrawable = drawable
+            if (bitmapDrawable.bitmap != null) {
+                return bitmapDrawable.bitmap
+            }
+        }
+        val bitmap: Bitmap = if (drawable.intrinsicWidth <= 0 || drawable.intrinsicHeight <= 0) {
+            Bitmap.createBitmap(
+                1,
+                1,
+                Bitmap.Config.ARGB_8888
+            ) // Single color bitmap will be created of 1x1 pixel
+        } else {
+            Bitmap.createBitmap(
+                drawable.intrinsicWidth,
+                drawable.intrinsicHeight,
+                Bitmap.Config.ARGB_8888
+            )
+        }
+        val canvas = Canvas(bitmap)
+        drawable.setBounds(0, 0, canvas.width, canvas.height)
+        drawable.draw(canvas)
+        return bitmap
     }
 
     private fun bindViews() {
         currentImage = findViewById(R.id.ivPhoto)
-        button = findViewById(R.id.btnGallery)
+        galleryBtn = findViewById(R.id.btnGallery)
+        brightnessSlider = findViewById(R.id.slBrightness)
     }
 
     // do not change this function
